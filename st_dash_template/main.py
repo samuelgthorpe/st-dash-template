1ce2bc60c362a731f3f9dc76cca80a6e219de95d
sam <samuelgthorpe@gmail.com>
2024-07-15 16:49:24 -0700

    functional open ai dashboard, still some minor issues with display

--- st_dash_template/main.py
+++ st_dash_template/main.py
@@ -11,32 +11,165 @@ Written by Samuel Thorpe
 
 # # Imports
 # -----------------------------------------------------|
-import argparse
-from dash import Dash, html
-from sampy.utils.logger import init_log
-from st_dash_template import BASE_DIR
+from resources.app import data, openai_client
+from markdown_strings import code_block
+from types import ModuleType
+import plotly
+import pandas  # noqa
+import numpy  # noqa
+import scipy  # noqa
+import stats  # noqa
+from dash import (
+    Dash,
+    html,
+    dash_table,
+    Input,
+    Output,
+    State,
+    callback,
+    dcc)
+
+
+# # Helper Methods
+# -----------------------------------------------------|
+def build_layout():
+    """Build the app layout."""
+    return [
+        dcc.Markdown(f'''
+            ### {data['dct']['meta']['name']} Dataset
+            {data['desc']}
+            '''),
+        dash_table.DataTable(
+            data=data['dfr'].to_dict('records'),
+            page_size=5
+            ),
+        html.Div([
+            html.Div(
+                'Input Command',
+                style={'whiteSpace': 'pre-line'}
+                ),
+            dcc.Textarea(
+                id='textarea-example',
+                value='input cmd',
+                style={'width': '100%', 'height': 30},
+                ),
+            html.Button(
+                'Submit',
+                id='textarea-state-example-button',
+                n_clicks=0),
+            html.Div(
+                '\nOutput',
+                style={'whiteSpace': 'pre-line'}
+                ),
+            html.Div(
+                id='textarea-example-output',
+                style={'whiteSpace': 'pre-line'}
+                )
+            ])
+    ]
+
+
+@callback(
+    Output('textarea-example-output', 'children'),
+    Input('textarea-state-example-button', 'n_clicks'),
+    State('textarea-example', 'value')
+)
+def update_output(n_clicks, value):
+    """Update the html output div."""
+    prompt = build_prompt_from_user_input(value)
+    response = get_completion(prompt)
+    children = handle_response(response)
+
+    if n_clicks > 0:
+        return children
+
+
+def build_prompt_from_user_input(value):
+    """Build the OpenAi prompt."""
+    return data["prompt"] + f' {value}'
+
+
+def get_completion(prompt):
+    """Call to OpenAi API to get completion."""
+    completion = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    response = completion.choices[0].message.content
+    return response
+
+
+def handle_response(response):
+    """Based on the completion content, build htm children."""
+    response = munge_validate_response(response)
+    evaluated = evaluate_response(response)
+
+    children = [
+        dcc.Markdown(
+            code_block(response, 'python'),
+            id='markdown-example-output',
+            style={
+                "white-space": "pre",
+                "overflow-x": "scroll"
+            }
+        )
+    ]
+
+    if isinstance(evaluated, plotly.graph_objects.Figure):
+        children.append(
+            dcc.Graph(figure=evaluated)
+            )
+    else:
+        children.append(
+            dcc.Markdown(
+                f'Evaluated Response: \n{evaluated}',
+                id='markdown-example-output',
+                style={
+                    "white-space": "pre",
+                    "overflow-x": "scroll"
+                    }
+                )
+            )
+
+    return children
+
+
+def evaluate_response(response):
+    """Evaluate the reponse as code and return output."""
+    try:
+        compiled = compile(response, 'multistring', 'exec')
+        module = ModuleType("testmodule")
+        exec(compiled, module.__dict__)
+        return module.response_function(data['dfr'])
+    except Exception as err:
+        return err.__str__()
+
+
+def munge_validate_response(response):
+    """Munge response for now. Eventually validate."""
+    def_idx = response.find("def")
+    if def_idx > 0:
+        response = response[def_idx:]
+
+    return_idx = response.find("return")
+    last_line = response[return_idx:]
+    last_line_split = last_line.split('\n')
+    if len(last_line_split) > 1:
+        response = response[:return_idx] + last_line_split[0]
+
+    return response
 
 
 # # Main Method
 # -----------------------------------------------------|
-def main(cfg_file, **kwrgs):
+def main():
     """Run main method."""
-    init_log(BASE_DIR)
     app = Dash()
-    app.layout = [html.Div(children='Hello World')]
-
-    return app
+    app.layout = build_layout()
+    app.run(debug=True)
 
 
 # # Main Entry
 # -----------------------------------------------------|
 if __name__ == "__main__":  # pragma: no cover
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-cfg',
-        type=str,
-        help='path to app cfg',
-        default="st_dash_template/cfg.yaml")
-    args = parser.parse_args()
-    app = main(args.cfg)
-    app.run(debug=True)
+    main()
---
+++ st_dash_template/resources/app.py
@@ -0,0 +1,41 @@
+"""
+Insert description.
+
+# NOTES
+# ----------------------------------------------------------------------------|
+
+
+Written July 14, 2024
+By Samuel Thorpe
+"""
+
+
+# # Imports
+# -----------------------------------------------------|
+import os
+from os.path import join
+from sampy.utils import load_yaml
+import pandas as pd
+from openai import OpenAI
+
+
+def load_txt(filename):
+    """Load text data."""
+    with open(filename) as fh:
+        return fh.read()
+
+
+def load_data():
+    """Load data."""
+    data_dir = 'data/bmi'
+    prompt_dir = 'templates/prompts/bmi'
+    return dict(
+        dfr=pd.read_csv(join(data_dir, 'dataframe.csv')),
+        dct=load_yaml(join(data_dir, 'dictionary.yaml')),
+        desc=load_txt(join(data_dir, 'description.txt')),
+        prompt=load_txt(join(prompt_dir, 'cmd.txt'))
+        )
+
+
+data = load_data()
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
